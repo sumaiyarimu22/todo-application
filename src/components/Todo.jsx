5e3a16ecfb4b3d4b5aa877344ff3c4c3c9481387
sumaiyarimu22 <sumaiyarimu22@gmail.com>
2023-10-12 09:57:06 +0600

    updateColor & update status to server data

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,41 +1,44 @@
+/* eslint-disable react/prop-types */
 import { useDispatch } from "react-redux";
 import cancelImage from "../assets/images/cancel.png";
-import { toggled, colorSelected, deleted } from "../redux/todos/actions";
+import { deleted } from "../redux/todos/actions";
+import updateTodo from "../redux/todos/thunk/updatestatus";
+import updateColor from "../redux/todos/thunk/updateColor";
 
 export default function Todo({ todo }) {
   const dispatch = useDispatch();
   const { id, text, completed, color } = todo;
 
   const handleStatusChange = (todoId) => {
-    dispatch(toggled(todoId));
+    dispatch(updateTodo(todoId, completed));
   };
 
   const handlerColorChange = (todoId, color) => {
-    dispatch(colorSelected(todoId, color));
+    dispatch(updateColor(todoId, color));
   };
 
   const handleDeleted = (todoId) => {
     dispatch(deleted(todoId));
   };
   return (
-    <div className="flex justify-start items-center p-2 hover:bg-gray-100 hover:transition-all space-x-4 border-b border-gray-400/20 last:border-0">
+    <div className='flex justify-start items-center p-2 hover:bg-gray-100 hover:transition-all space-x-4 border-b border-gray-400/20 last:border-0'>
       <div
-        className={`rounded-full bg-white border-2 border-gray-400 w-5 h-5 flex flex-shrink-0 justify-center items-center mr-2 ${
+        className={`rounded-full bg-white border-2 relative border-gray-400 w-5 h-5 flex flex-shrink-0 justify-center items-center mr-2 ${
           completed && `border-green-500 focus-within:border-green-500`
         }`}
       >
         <input
-          type="checkbox"
-          className="opacity-0 absolute rounded-full"
+          type='checkbox'
+          className='opacity-0 absolute rounded-full'
           checked={completed}
           onChange={() => handleStatusChange(id)}
         />
         {completed && (
           <svg
-            className="fill-current w-3 h-3 text-green-500 pointer-events-none"
-            viewBox="0 0 20 20"
+            className='fill-current w-3 h-3 text-green-500 pointer-events-none'
+            viewBox='0 0 20 20'
           >
-            <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
+            <path d='M0 11l2-2 5 5L18 3l2 2L7 18z' />
           </svg>
         )}
       </div>
@@ -67,8 +70,8 @@ export default function Todo({ todo }) {
 
       <img
         src={cancelImage}
-        className="flex-shrink-0 w-4 h-4 ml-2 cursor-pointer"
-        alt="Cancel"
+        className='flex-shrink-0 w-4 h-4 ml-2 cursor-pointer'
+        alt='Cancel'
         onClick={() => handleDeleted(id)}
       />
     </div>
